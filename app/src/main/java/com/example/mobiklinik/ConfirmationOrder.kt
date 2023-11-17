e12db5a1a95457324ffb9a4872b27cc42b8c9528
Yilian90 <chenyilian90@gmail.com>
2023-11-17 11:50:04 +0800

    add confirmation order

---
+++ app/src/main/java/com/example/mobiklinik/ConfirmationOrder.kt
@@ -0,0 +1,13 @@
+package com.example.mobiklinik
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConfirmationOrder  : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.confirmation_order)
+
+    }
+}
